62b340651ee19c3660872ae4b7db6a5b31d6bacb
ChairmanPandaCN <lusi@tcd.ie>
2020-07-22 20:51:41 +0800

    sync

---
+++ 2.Hello Haskell/letAndWhere.hs
@@ -0,0 +1,12 @@
+mult1 = x * 3 +y
+    where x = 3
+          y = 1000
+
+mult2 = x * 5
+    where y = 10
+          x = 10 * 5 + y
+
+mult3 = z / x + y
+    where x = 7
+          y = -7
+          z = y * 10
